7156ebb4f8ecd39371c1c19cf6de91fd0f47d5d0
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 12:43:45 -0400

    Implement solution for 15_intersection

--- 15_intersection/intersection.py
+++ 15_intersection/intersection.py
@@ -1,15 +1,25 @@
 def intersection(l1, l2):
     """Return intersection of two lists as a new list::
-    
+
         >>> intersection([1, 2, 3], [2, 3, 4])
         [2, 3]
-        
+
         >>> intersection([1, 2, 3], [1, 2, 3, 4])
         [1, 2, 3]
-        
+
         >>> intersection([1, 2, 3], [3, 4])
         [3]
-        
+
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+
+    # convert each list to set
+    set1 = set(l1)
+    set2 = set(l2)
+
+    # apply intersection on set1 and set2 and store the result
+    result = set1 & set2
+
+    # convert the result to a list and return
+    return list(result)
