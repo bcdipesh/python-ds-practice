185b1a4acb8c635306376c722a595fd745090fba
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 15:19:46 -0400

    Implement solution for 23_list_check

--- 23_list_check/list_check.py
+++ 23_list_check/list_check.py
@@ -7,3 +7,13 @@ def list_check(lst):
         >>> list_check([[1], "nope"])
         False
     """
+
+    # loop over all elements in the list
+    for element in lst:
+        # for each element check if it is of type list
+        if isinstance(element, list) != True:
+            # if that is not the case return False
+            return False
+
+    # if code reached this line then return True
+    return True
