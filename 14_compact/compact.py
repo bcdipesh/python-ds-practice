041de4eb492663ced8baad20bf2e0a1ed7a18713
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 12:39:46 -0400

    Implement solution for 14_compact

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -4,3 +4,8 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+
+    # use list comprehension and create a new list that has truthy value and return it
+    true_elements = [el for el in lst if el]
+
+    return true_elements
