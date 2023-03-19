302f922a290faaa85c1e1954110d35597c7ddf2d
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-18 21:05:56 -0400

    Implement solution for 07_multiple_letter_count

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,8 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+
+    # convert phrase into a list
+    phrase_list = list(phrase)
+    # use list count method and dictionary comprehension to generate and return the desired output
+    return {char: phrase_list.count(char) for char in phrase_list}
