a4fe248c4f038638a2251facdd69c46a7f29ea66
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 12:02:49 -0400

    Implement solution for 10_frequence

--- 10_frequency/frequency.py
+++ 10_frequency/frequency.py
@@ -1,9 +1,12 @@
 def frequency(lst, search_term):
     """Return frequency of term in lst.
-    
+
         >>> frequency([1, 4, 3, 4, 4], 4)
         3
-        
+
         >>> frequency([1, 4, 3], 7)
         0
     """
+
+    # use list count method to count the number of occurrence of the search_term and return it
+    return lst.count(search_term)
