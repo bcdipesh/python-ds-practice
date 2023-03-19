307cb4ce05e03da8731f1980f46d894a894a7614
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-18 20:57:21 -0400

    Implement solution for 05_reverse_string

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -7,3 +7,8 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+
+    # turn the string into individual characters list
+    str_list = list(phrase)
+    # get characters from last to first with list slicing, join the resulting list into new str and return
+    return "".join(str_list[::-1])
