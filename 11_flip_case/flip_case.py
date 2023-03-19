fa13875f2314f2db83c1f3570e9711ada4971745
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 12:14:38 -0400

    Implement solution for 11_flip_case

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,20 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+
+    # convert the phrase to a list and store it in phrase_list
+    phrase_list = list(phrase)
+    swapped_phrase_list = []
+
+    # loop through the phrase_list
+    for ch in phrase_list:
+        # for each character in the list uppercase both the character and to_swap and compare if both are equal
+        if ch.upper() == to_swap.upper():
+            # if this is the case swap the case of the character and add it to swapped_phrase_list
+            swapped_phrase_list.append(ch.swapcase())
+        # else just add the character as it is to swapped_phrase_list
+        else:
+            swapped_phrase_list.append(ch)
+
+    # join the items inside the swapped_phrase_list and return it as a string
+    return ''.join(swapped_phrase_list)
