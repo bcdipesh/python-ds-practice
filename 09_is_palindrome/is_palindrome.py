fa1726f6bd07039b4d467e7990817a3874d548c8
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 12:00:37 -0400

    Implement solution for 09_is_palindrome

--- 09_is_palindrome/is_palindrome.py
+++ 09_is_palindrome/is_palindrome.py
@@ -21,3 +21,30 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
+
+    # convert phrase to lowercase
+    phrase = phrase.lower()
+
+    # strip leading and trailing white spaces
+    phrase = phrase.strip()
+
+    # split the phrase on space into word list and store it in word_list
+    word_list = phrase.split(' ')
+
+    # check if the size of word_list is 1
+    if len(word_list) == 1:
+        # if this is the case phrase has no spaces and we need to split the phrase into individual characters
+        word_list = list(phrase)
+
+    # join the words on the list without space and store in phrase
+    phrase = ''.join(word_list)
+
+    # make a list of characters from the updated phrase and reverse it
+    reversed_phrase = list(phrase)
+    reversed_phrase.reverse()
+
+    # join the reversed_phrase
+    reversed_phrase = ''.join(reversed_phrase)
+
+    # check and return True if phrase and reversed_phrase are equal else False
+    return phrase == reversed_phrase
