b601e0c0076cb009710d60726c66097bbb32f536
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-18 20:44:31 -0400

    Implement solution for 03_last_element

--- 03_last_element/last_element.py
+++ 03_last_element/last_element.py
@@ -1,9 +1,16 @@
 def last_element(lst):
     """Return last item in list (None if list is empty.
-    
+
         >>> last_element([1, 2, 3])
         3
-        
+
         >>> last_element([]) is None
         True
     """
+
+    # check if list is empty
+    if len(lst) == 0:
+        # if it is empty return None
+        return None
+    # if not empty pop the last element from the list
+    return lst.pop()
