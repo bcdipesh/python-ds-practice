32d20cdc3dcaf8af08c4de60cf355ec4ddc5732b
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-18 21:02:18 -0400

    Implement solution for 06_single_letter_count

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -1,12 +1,20 @@
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
-    
+
         >>> single_letter_count('Hello World', 'h')
         1
-        
+
         >>> single_letter_count('Hello World', 'z')
         0
-        
+
         >>> single_letter_count("Hello World", 'l')
         3
     """
+
+    # uppercase the word and convert it to a list
+    word_list = list(word.upper())
+    # uppercase the letter
+    letter = letter.upper()
+    # use list count method to count the occurrence of letter in the list
+    # return the count
+    return word_list.count(letter)
