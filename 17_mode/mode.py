b0880aa8482b1df7ef6b23257d7e52c7b98116d8
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-19 13:09:17 -0400

    Implement solution for 17_mode

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,23 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+
+    # make an empty dictionary
+    count = {}
+
+    # loop through each number in the nums
+    for num in nums:
+        # for each num check if the num is not present as a key in the dictionary
+        if num not in count:
+            # if this is the case create a key/value pair in dictionary where the key is the current num and the value is its total count in list
+            count[num] = nums.count(num)
+
+    # make a variable called most_common
+    most_common = 0
+
+    # loop through the dictionary keys and return the key that has the greatest value
+    for num in count.keys():
+        if most_common < count[num]:
+            most_common = num
+
+    return most_common
