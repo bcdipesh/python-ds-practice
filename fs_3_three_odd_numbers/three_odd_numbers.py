48e9f3a724112df30bd6695315f824f3b1ce1a14
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-24 15:25:45 -0400

    Implement solution for fs_3_three_odd_numbers

--- fs_3_three_odd_numbers/three_odd_numbers.py
+++ fs_3_three_odd_numbers/three_odd_numbers.py
@@ -13,3 +13,23 @@ def three_odd_numbers(nums):
         >>> three_odd_numbers([1, 2, 3, 3, 2])
         False
     """
+
+    # loop over nums from the start index
+    for i in range(0, len(nums)):
+        # create a variable named total and set it to 0
+        total = 0
+        # loop over nums from start index to start index + 3
+        for j in range(i, i + 3):
+            # check if index is out of range
+            if ((i + 3) - 1) > len(nums) - 1:
+                # if that is the case break from current loop
+                break
+            # sum numbers with in the range
+            else:
+                total += nums[j]
+        # if the total is odd return True
+        if total % 2 != 0:
+            return True
+
+    # if code reaches here return False
+    return False
