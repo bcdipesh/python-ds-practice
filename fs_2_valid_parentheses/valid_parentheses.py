39120e9057209f6bcaadac953b8ee2455e396bd2
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-24 15:06:44 -0400

    Implement solution for fs_2_valid_parentheses

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,14 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+
+    # create a paren_count dictionary and initialize it with '(': 0 , ')':0
+    paren_count = {'(': 0, ')': 0}
+
+    # loop over each paren in the parens
+    for paren in parens:
+        # for each paren increase the corresponding value in pren_count
+        paren_count[paren] = paren_count[paren] + 1
+
+    # compare the value of '(' and ')' and return the result
+    return paren_count['('] == paren_count[')']
