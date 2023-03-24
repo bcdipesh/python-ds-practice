850dbe54dcb10d4e9316b134f676fa2e4dd82a10
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-24 12:22:50 -0400

    Implement solution for 28_find_factors

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,16 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    # create an empty list called factors
+    factors = []
+
+    # run a loop through 1 to num
+    for number in range(1, num + 1):
+        # check if current number is a factor of num
+        if num % number == 0:
+            # if that is the case add the number to factors
+            factors.append(number)
+
+    # return the factors
+    return factors
