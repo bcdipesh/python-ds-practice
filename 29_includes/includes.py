56713d3cb89c2ef799703200ab2eac1d99d67e6d
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-24 12:44:35 -0400

    Implement solution for 29_includes

--- 29_includes/includes.py
+++ 29_includes/includes.py
@@ -30,3 +30,39 @@ def includes(collection, sought, start=None):
         >>> includes({"apple": "red", "berry": "blue"}, "blue")
         True
     """
+
+    # check the type of collection
+    if isinstance(collection, (str, list, tuple)):
+        # if it is a string, list or tuple check if start index is provided
+        if start is not None:
+            # if that is the case start searching from that index
+            # loop through the collection starting from the given index till end
+            for index in range(start, len(collection)):
+                # for each element check if it matches the sought element
+                if collection[index] == sought:
+                    # if it does return True
+                    return True
+        # else start searching from the start
+        else:
+            # loop through the collection till the end
+            for element in collection:
+                # for each element check if it matches the sought element
+                if element == sought:
+                    # if it does return True
+                    return True
+    # elif check if the collection is a set
+    elif isinstance(collection, set):
+        # if that is the case return True if sought value is present
+        if sought in collection:
+            return True
+    # elif check if the collection is a dictionary
+    elif isinstance(collection, dict):
+        # if that is the case loop over the values of the dict
+        for value in collection.values():
+            # for each value check if it matches the sought value
+            if value == sought:
+                # if it does return True
+                return True
+
+    # if code reached this line return False
+    return False
