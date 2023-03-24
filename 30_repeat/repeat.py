6dead903b4c8103e5f91b7e26aac9b4c4b971422
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-24 12:55:11 -0400

    Implement solution for 30_repeat

--- 30_repeat/repeat.py
+++ 30_repeat/repeat.py
@@ -18,3 +18,18 @@ def repeat(phrase, num):
         >>> repeat('abc', 'nope') is None
         True
     """
+
+    # check if num is a valid number and greater than or equal to 0
+    if isinstance(num, int) and num >= 0:
+        # if that is the case create a empty list called phrase_list
+        phrase_list = []
+
+        # loop for num times and append phrase to phrase_list for each iteration
+        for _ in range(0, num):
+            phrase_list.append(phrase)
+
+        # create and return a str using join on phrase_list
+        return ''.join(phrase_list)
+    # else return None
+    else:
+        return None
